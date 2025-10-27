d8c8f01aa7a082ce0c8be01e1b572339bf14fe86
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2025-10-27 14:36:13 +0300

    modularize Imports for more performance

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
 });
 
 export default withBundleAnalyzer({
   reactStrictMode: true,
   swcMinify: true,
+  modularizeImports: {
+    "react-icons/fa6": { transform: "react-icons/fa6/{{member}}" },
+    "lucide-react": { transform: "lucide-react/{{member}}" },
+    "framer-motion": { transform: "framer-motion/{{member}}" },
+  },
 });
