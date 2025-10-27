525f1e6dc6f6cc15b157d25ae48bb1e27f18bcbf
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2025-10-27 10:53:39 +0300

    Update default language

--- public/scripts/lang-init.js
+++ public/scripts/lang-init.js
@@ -2,5 +2,5 @@
   const stored = localStorage.getItem("lang");
   const lang = stored || (navigator.language.startsWith("ar") ? "ar" : "ar");
   document.documentElement.setAttribute("lang", lang);
-  document.documentElement.setAttribute("dir", lang === "en" ? "ltr" : "rtl");
+  document.documentElement.setAttribute("dir", lang === "ar" ? "rtl" : "ltr");
 })();
